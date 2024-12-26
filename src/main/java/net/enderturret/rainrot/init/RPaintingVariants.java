f25c0a52cea733481307764ed075316d5a94e88a
EnderTurret <36442079+EnderTurret@users.noreply.github.com>
2024-12-25 20:13:26 -0500

    Fix painting sizes.

--- src/main/java/net/enderturret/rainrot/init/RPaintingVariants.java
+++ src/main/java/net/enderturret/rainrot/init/RPaintingVariants.java
@@ -13,20 +13,20 @@ public final class RPaintingVariants {
 	public static final DeferredRegister<PaintingVariant> REGISTRY = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, RainRot.MOD_ID);
 
 	public static final RegistryObject<PaintingVariant> ITERATORS_AGAINST_BIOENGINEERING = REGISTRY.register("iterators_against_bioengineering",
-			() -> new PaintingVariant(2, 2));
+			() -> new PaintingVariant(32, 32));
 
 	public static final RegistryObject<PaintingVariant> KARMA_COMPANIONSHIP = REGISTRY.register("karma_companionship",
-			() -> new PaintingVariant(4, 4));
+			() -> new PaintingVariant(64, 64));
 
 	public static final RegistryObject<PaintingVariant> KARMA_GLUTTONY = REGISTRY.register("karma_gluttony",
-			() -> new PaintingVariant(4, 4));
+			() -> new PaintingVariant(64, 64));
 
 	public static final RegistryObject<PaintingVariant> KARMA_LUST = REGISTRY.register("karma_lust",
-			() -> new PaintingVariant(4, 4));
+			() -> new PaintingVariant(64, 64));
 
 	public static final RegistryObject<PaintingVariant> KARMA_SURVIVAL = REGISTRY.register("karma_survival",
-			() -> new PaintingVariant(4, 4));
+			() -> new PaintingVariant(64, 64));
 
 	public static final RegistryObject<PaintingVariant> KARMA_VIOLENCE = REGISTRY.register("karma_violence",
-			() -> new PaintingVariant(4, 4));
+			() -> new PaintingVariant(64, 64));
 }
